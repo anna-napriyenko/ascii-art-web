8bc0e4d8bebaebc421f971d4edb5ca0731a3be86
anna-napriyenko <naprieynko.anna@gmail.com>
2025-08-17 15:32:56 +0500

    jsonlearning

---
+++ json/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	encodeJson()
+	decodeJson()
+	decodeIntoMap()
+}
+
+func encodeJson() {
+	person := Person{Name: "John", Age: 32}
+	jsonData, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Encode JSON:")
+	fmt.Println(string(jsonData))
+}
+
+func decodeJson() {
+	jsonData := []byte(`{"name":"John","age":32}`)
+
+	var person Person
+	err := json.Unmarshal(jsonData, &person)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Decode JSON")
+	fmt.Println(person)
+}
+
+func decodeIntoMap() {
+	jsonData := []byte(`{"name":"John","age":32}`)
+
+	var data map[string]interface{}
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("\nDecode JSON into map: ")
+	fmt.Println(data)
+}
